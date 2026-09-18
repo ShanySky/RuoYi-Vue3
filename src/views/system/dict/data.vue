2a595acb9702a0b56f0415636cd4699653d53f51
Shane <61485912+ShanySky@users.noreply.github.com>
2026-09-18 20:06:40 +0800

    fix(ai): import page capability registry explicitly

--- src/views/system/dict/data.vue
+++ src/views/system/dict/data.vue
@@ -180,6 +180,7 @@ import useDictStore from '@/store/modules/dict'
 import { optionselect as getDictOptionselect, getType } from "@/api/system/dict/type"
 import { listData, getData, delData, addData, updateData } from "@/api/system/dict/data"
 import { createAiCrudPageCapabilities } from "@/ai/crudPageCapabilities"
+import { useAiPageTools } from "@/ai/toolRegistry"
 
 const { proxy } = getCurrentInstance()
 const { sys_normal_disable } = useDict("sys_normal_disable")
